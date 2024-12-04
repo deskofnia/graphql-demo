5e043d8592ec79c93a9101439ed3bffadd4dde18
devendra <devendra@biz4group.com>
2024-12-04 17:19:21 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import './App.css';
+
+const GET_LOCATIONS = gql`
+  query GetTodos {
+    getTodos {
+      id,
+      title,
+      userId,
+      user {
+        email
+      }
+    },
+  }
+`;
+
+function DisplayTodos() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return <div style={{ flexDirection: "row", justifyItems: "center", alignItems: "center" }}>
+    <table style={{ borderCollapse: "collapse", textAlign: "center", width: "80%" }}>
+      <tr>
+        <th>Id</th>
+        <th>Todo Name</th>
+        <th>User Email</th>
+      </tr>
+      {data.getTodos.map(({ id, title, user }: any) => (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{title}</td>
+          <td>{user?.email}</td>
+        </tr>
+      ))}
+    </table>
+  </div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayTodos />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.ts
@@ -0,0 +1,70 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+import axios from "axios";
+import cors from "cors";
+
+const schema = buildSchema(`
+    type User {
+        username: String!
+        email: String!
+        phone: String!
+        website: String!
+    }
+    type Todo {
+        id: ID!
+        title: String!
+        completed: Boolean
+        userId: ID!
+        user: User
+    }
+    type Query {
+        getTodos: [Todo]
+        getTodoById(id: ID!): Todo
+    }
+`);
+
+const root = {
+  getTodos: async () => {
+    const todos = (
+      await axios.get("https://jsonplaceholder.typicode.com/todos")
+    ).data;
+    const users = (
+      await axios.get("https://jsonplaceholder.typicode.com/users")
+    ).data;
+
+    // Map over todos to embed the user object
+    return todos.map((todo) => ({
+      ...todo,
+      user: users.find((user) => user.id === todo.userId),
+    }));
+  },
+  getTodoById: async ({ id }) => {
+    const todo = (
+      await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    ).data;
+    const user = (
+      await axios.get(
+        `https://jsonplaceholder.typicode.com/users/${todo.userId}`
+      )
+    ).data;
+
+    // Add the user object to the todo
+    return { ...todo, user };
+  },
+};
+
+const app = express();
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000);
+
+console.log("GraphQL API server available at http://localhost:4000/graphql");
